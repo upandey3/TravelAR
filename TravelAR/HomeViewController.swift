ea4889f41a7af17cf0c92f352d6a0fe8b140d2ed
Aakash Patel <aakashpatel@Aakashs-Mac.local>
2017-09-17 07:18:31 -0700

    MS

---
+++ TravelAR/HomeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  HomeViewController.swift
+//  TravelAR
+//
+//  Created by Aakash Patel on 9/17/17.
+//  Copyright © 2017 Utkarsh Pandey. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+      
+    }
+ 
+
+}
